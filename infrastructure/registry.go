27b03d5e8874b0effb97e2b04c80cfb0c1a17665
zjk <zjkmmd@163.com>
2025-11-16 00:02:22 +0800

    调整

--- infrastructure/registry.go
+++ infrastructure/registry.go
@@ -1,6 +1,7 @@
 package infrastructure
 
 import (
+	"fmt"
 	"github.com/dhlanshan/otp/dto"
 	"github.com/dhlanshan/otp/enum"
 	"github.com/dhlanshan/otp/internal/abstract"
@@ -18,15 +19,26 @@ var (
 )
 
 // RegisterPattern 注册模式
-func RegisterPattern[T any](pattern enum.PatternEnum, obj abstract.Pattern) {
+func RegisterPattern[T any](patternName enum.PatternEnum, obj abstract.Pattern, _ T) {
 	registryMu.Lock()
 	defer registryMu.Unlock()
+
+	// 强制要求 obj 必须实现 Pattern
+	var _ abstract.Pattern = obj
 
 	var zero T
 	t := reflect.TypeOf(zero)
 
-	PatternMap[pattern] = obj
-	PatternArgMap[pattern] = t
+	// t 为 struct 或 *struct
+	if t.Kind() == reflect.Pointer {
+		t = t.Elem()
+	}
+	if t.Kind() != reflect.Struct {
+		panic(fmt.Errorf("Pattern(%v) 的参数类型必须为 struct 或 struct 指针，实际: %v", patternName, t.Kind()))
+	}
+
+	PatternMap[patternName] = obj
+	PatternArgMap[patternName] = t
 }
 
 func GetPatternArg(pattern enum.PatternEnum) reflect.Type {
